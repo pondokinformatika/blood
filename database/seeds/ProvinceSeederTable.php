b814938364c5701507a61d55f7e9a923e9c5d3ae
masebilal07 <masebilal2307@gmail.com>
2018-11-24 05:33:00 +0700

    seeder provinci

--- database/seeds/ProvinceSeederTable.php
+++ database/seeds/ProvinceSeederTable.php
@@ -1,3 +1,4 @@
+
 <?php
 
 use Illuminate\Database\Seeder;
@@ -17,40 +18,177 @@ class ProvinceSeederTable extends Seeder
     {
         Province::truncate();
 		Province::insert([
-		      	['name' => 'ACEH'],
-		       	['name' => 'SUMATERA UTARA'],
-		       	['name' => 'SUMATERA BARAT'],
-				['name' => 'RIAU'],
-				['name' => 'JAMBI'],
-				['name' => 'SUMATERA SELATAN'],
-				['name' => 'BENGKULU'],
-				['name' => 'LAMPUNG'],
-				['name' => 'KEPULAUAN BANGKA BELITUNG'],
-				['name' => 'KEPULAUAN RIAU'],
-				['name' => 'DKI JAKARTA'],
-				['name' => 'JAWA BARAT'],
-				['name' => 'JAWA TENGAH'],
-				['name' => 'DI YOGYAKARTA'],
-				['name' => 'JAWA TIMUR'],
-				['name' => 'BANTEN'], 
-				['name' => 'BALI'], 
-				['name' => 'NUSA TENGGARA BARAT'],
-				['name' => 'NUSA TENGGARA TIMUR'],
-				['name' => 'KALIMANTAN BARAT'],
-				['name' => 'KALIMANTAN TENGAH'],
-				['name' => 'KALIMANTAN SELATAN'],
-				['name' => 'KALIMANTAN TIMUR'],
-				['name' => 'KALIMANTAN UTARA'],
-				['name' => 'SULAWESI UTARA'],
-				['name' => 'SULAWESI TENGAH'],
-				['name' => 'SULAWESI SELATAN'],
-				['name' => 'SULAWESI TENGGARA'],
-				['name' => 'GORONTALO'],
-				['name' => 'SULAWESI BARAT'],
-				['name' => 'MALUKU'],
-				['name' => 'MALUKU UTARA'],
-				['name' => 'PAPUA BARAT'],
-				['name' => 'PAPUA']
+		      	[
+		      		'name' => 'ACEH',
+		      		'name' => 'DKI JAKARTA',
+					'latitude' => '5.276281',
+					'longitude' => '95.725866' 
+		      	],
+		       	[
+		       		'name' => 'SUMATERA UTARA',
+		       		'latitude' => null,
+		       		'longitude' => null
+		       	],
+		       	[
+		       		'name' => 'SUMATERA BARAT',
+		       		'latitude' => null,
+		       		'longitude' => null
+		       	],
+				[
+					'name' => 'RIAU',
+					'latitude' => null,
+					'longitude' => null
+				],
+				[
+					'name' => 'JAMBI',
+					'latitude' => null,
+					'longitude' => null
+				],
+				[
+					'name' => 'SUMATERA SELATAN',
+					'latitude' => null,
+					'longitude' => null
+				],
+				[
+					'name' => 'BENGKULU',
+					'latitude' => null,
+					'longitude' => null
+				],
+				[
+					'name' => 'LAMPUNG',
+					'latitude' => null,
+					'longitude' => null
+				],
+				[
+					'name' => 'KEPULAUAN BANGKA BELITUNG',
+					'latitude' => null,
+					'longitude' => null
+				],
+				[
+					'name' => 'KEPULAUAN RIAU',
+					'latitude' => null,
+					'longitude' => null
+				],
+				[
+					'name' => 'DKI JAKARTA',
+					'latitude' => '-6.156563',
+					'longitude' => '106.856935'
+				],
+				[
+					'name' => 'JAWA BARAT',
+					'latitude' => '-6.923893',
+					'longitude' => '107.615348'
+				],
+				[
+					'name' => 'JAWA TENGAH',
+					'latitude' => '-7.249743', 
+					'longitude' => '110.135208'
+				],
+				[
+					'name' => 'DI YOGYAKARTA',
+					'latitude' => '-7.923188', 
+					'longitude' => '110.447682' 
+				],
+				[
+					'name' => 'JAWA TIMUR',
+					'latitude' => null,
+					'longitude' => null
+				],
+				[
+					'name' => 'BANTEN',
+					'latitude' => null,
+					'longitude' => null
+				], 
+				[
+					'name' => 'BALI',
+					'latitude' => '-8.312976', 
+					'longitude' => '115.272104'
+				], 
+				[
+					'name' => 'NUSA TENGGARA BARAT',
+					'latitude' => null,
+					'longitude' => null
+				],
+				[
+					'name' => 'NUSA TENGGARA TIMUR',
+					'latitude' => null,
+					'longitude' => null
+				],
+				[
+					'name' => 'KALIMANTAN BARAT',
+					'latitude' => null,
+					'longitude' => null
+				],
+				[
+					'name' => 'KALIMANTAN TENGAH',
+					'latitude' => null,
+					'longitude' => null
+				],
+				[
+					'name' => 'KALIMANTAN SELATAN',
+					'latitude' => null,
+					'longitude' => null
+				],
+				[
+					'name' => 'KALIMANTAN TIMUR',
+					'latitude' => null,
+					'longitude' => null
+				],
+				[
+					'name' => 'KALIMANTAN UTARA',
+					'latitude' => null,
+					'longitude' => null
+				],
+				[
+					'name' => 'SULAWESI UTARA',
+					'latitude' => null,
+					'longitude' => null
+				],
+				[
+					'name' => 'SULAWESI TENGAH',
+					'latitude' => null,
+					'longitude' => null
+				],
+				[
+					'name' => 'SULAWESI SELATAN',
+					'latitude' => null,
+					'longitude' => null
+				],
+				[
+					'name' => 'SULAWESI TENGGARA',
+					'latitude' => null,
+					'longitude' => null
+				],
+				[
+					'name' => 'GORONTALO',
+					'latitude' => null,
+					'longitude' => null
+				],
+				[
+					'name' => 'SULAWESI BARAT',
+					'latitude' => null,
+					'longitude' => null
+				],
+				[
+					'name' => 'MALUKU',
+					'latitude' => null,
+					'longitude' => null
+				],
+				[
+					'name' => 'MALUKU UTARA',
+					'latitude' => null,
+					'longitude' => null
+				],
+				[
+					'name' => 'PAPUA BARAT',
+					'latitude' => null,
+					'longitude' => null
+				],
+				[
+					'name' => 'PAPUA',
+					'latitude' => null,
+					'longitude' => null
+				]
 
 		]);
     }
