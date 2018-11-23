23600f738346c62386d096278331103915b9c5bc
masebilal07 <masebilal2307@gmail.com>
2018-11-24 05:01:31 +0700

    menampilkan pendonor

---
+++ resources/views/admin/pendonor/table.blade.php
@@ -0,0 +1,30 @@
+<table class="table table-hover">
+  
+  <tr>
+    <th width="40">#</th>
+    <th width="200">Nama Pendonor</th>
+    <th>Golongan Darah</th>
+    <th>No Telepon</th>
+    <th width="150">Action</th>
+  </tr>
+
+  <?php $no = 1; ?>
+  @foreach($pendonors as $pendonor)
+    <tr>
+      <td>{{ $no++ }}</td>
+      <td>{{ $pendonor->name }}</td>
+      <td>{{ $pendonor->blood_type_id }}</td>
+      <td>{{ $pendonor->phone }}</td>
+      <td>
+        <form action="{{route('pendonor.destroy', $pendonor->id)}}" method="post">
+          {{ csrf_field() }}
+          {{ method_field("DELETE") }}
+          <button onclick="return confirm('Are you sure?')" type="submit" class="btn btn-xs btn-danger">
+            <i class="fa fa-trash"></i> Hapus
+          </button>
+        </form>
+      </td>
+    </tr>
+  @endforeach
+
+</table>
