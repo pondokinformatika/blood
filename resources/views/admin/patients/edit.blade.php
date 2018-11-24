1ab68a4cbba83b18be04ab5bf68c59987aa03ea7
Nuli <giarsyani.nuli@gmail.com>
2018-11-24 07:12:00 +0700

    update edit patient

--- resources/views/admin/patients/edit.blade.php
+++ resources/views/admin/patients/edit.blade.php
@@ -3,6 +3,12 @@
 @section('title', 'Patients')
 
 @section('content')
+<div class="box">
+    <div class="box-header with-border">
+      <h3 class="box-title">{{ $patient->exists ? 'Edit' : 'Tambah' }} Pasien</h3>
+      <span class="text-muted small pull-right">kolom yang memiliki tanda ( * ) wajib diisi!</span>
+    </div>
+    <div class="box-body">
     {!! Form::model($patient, [
       'method'  => 'PUT',
       'route'   => ['patients.update', $patient->id],
@@ -12,4 +18,8 @@
     @include('admin.patients.form')
 
     {!! Form::close() !!}
+    </div>
+    <!-- /.box-body -->
+        <!-- /.box-footer-->
+</div>
 @endsection
